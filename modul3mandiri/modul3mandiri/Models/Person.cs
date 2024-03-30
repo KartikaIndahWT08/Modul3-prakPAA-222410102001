a8fd6220830f4a4dd7208b7e2f8e0a4552ef0ee0
KartikaIndahWT08 <117880126+KartikaIndahWT08@users.noreply.github.com>
2024-03-31 03:17:05 +0700

    Add files via upload

---
+++ modul3mandiri/modul3mandiri/Models/Person.cs
@@ -0,0 +1,11 @@
+﻿namespace modul3mandiri.Models
+{
+    public class Mahasiswa
+    {
+        public int id_person { get; set; }
+        public string nama { get; set; }
+        public string alamat { get; set; }
+        public string email { get; set; }
+    }
+
+}
