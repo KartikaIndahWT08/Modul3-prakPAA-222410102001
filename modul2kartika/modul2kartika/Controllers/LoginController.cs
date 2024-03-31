0a1836bffef57e9dc662773899de3a920f5971dc
KartikaIndahWT08 <117880126+KartikaIndahWT08@users.noreply.github.com>
2024-03-31 09:43:00 +0700

    Add files via upload

---
+++ modul2kartika/modul2kartika/Controllers/LoginController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using modul2kartika.Models;
+using modul2kartika.Models;
+
+namespace modul2kartika.Controllers
+{
+    public class LoginController : Controller
+    {
+        private string __constr;
+        private IConfiguration __config;
+
+        public LoginController(IConfiguration configuration)
+        {
+            __config = configuration;
+            __constr = configuration.GetConnectionString("WebApiDatabase");
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost("API/LOGIN")]
+
+        public IEnumerable<Login> LoginUser(string namaUser, string password)
+        {
+            LoginContext context = new LoginContext(this.__constr);
+            List<Login> listLogin = context.Authentifikasi(namaUser, password, __config);
+            return (listLogin).ToArray();
+        }
+    }
+}
